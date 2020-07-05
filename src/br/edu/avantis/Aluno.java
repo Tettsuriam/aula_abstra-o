7f35939922cd6506855de308f30d228236175cf2
João Pedro Nascimento Braz <joao2012pedro2012@gmail.com>
2020-07-05 15:13:42 -0300

    deu boa

---
+++ src/br/edu/avantis/Aluno.java
@@ -0,0 +1,26 @@
+package br.edu.avantis;
+
+public class Aluno {
+	
+	String nome;
+	int ra; //código de aluno
+	String email;
+	
+	//estuda
+	//faz prova
+	//frequenta aulas
+	//diverti
+	
+	Conhecimento estuda() {
+		return new Conhecimento();
+	}
+	
+	NotaAvaliacao fazProva() {
+		return null;
+	}
+	
+	void frequentaAula() {
+		
+	}
+	
+}
